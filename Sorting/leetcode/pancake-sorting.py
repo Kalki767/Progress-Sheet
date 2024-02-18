40f4efcdb49fb08700835d6060e1eb5dd977a450
Kalki767 <137619853+Kalki767@users.noreply.github.com>
2024-02-18 22:26:09 +0300

    Add solution for Pancake Sorting

--- Sorting/leetcode/pancake-sorting.py
+++ Sorting/leetcode/pancake-sorting.py
@@ -14,12 +14,21 @@ class Solution:
         the largest element from the unsorted list'''
         for index in range(n):
             largest_index = arr.index(n - index)
+
+            '''Step3: if the current element is already on it's desired
+            place don't flip anything'''
             if largest_index == n-1-index:
                 continue
+
             elif largest_index == 0:
+                '''Step4: if the current element is on the first list then we
+            need to do only one flip'''
                 arr = list(reversed(arr[:n - index])) + arr[n - index:]
                 result.append(n - index)
             else:
+                '''Step5: we need to reverse the first elements to get the
+                current element to the first then we can flip it to put it
+                back to it's position'''
                 reversed_array = list(reversed(arr[:largest_index + 1])) + arr[largest_index+1:]
                 arr = list(reversed(reversed_array[:n - index])) + arr[n - index :]
                 result.extend([largest_index + 1, n - index])
