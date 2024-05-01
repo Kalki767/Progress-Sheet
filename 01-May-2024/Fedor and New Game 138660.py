0252a77ec4ef9ca7cc7a9ce389ff57d8ba53727d
Kalkidan Kidane <137619853+Kalki767@users.noreply.github.com>
2024-05-01 21:24:33 +0300

    Created 'Fedor and New Game' by A2SV Hub

---
+++ 01-May-2024/Fedor and New Game 138660.py
@@ -0,0 +1,19 @@
+# Problem: Fedor and New Game - https://codeforces.com/contest/467/problem/B
+
+n, m, k = map(int,input().split())
+soldiers = []
+for _ in range(m+1):
+    sol = int(input())
+    soldiers.append(sol)
+answer = 0
+for index in range(m):
+    sol = soldiers[index]
+    xor = sol ^ soldiers[-1]
+    diff = 0
+    while xor > 0:
+        if xor & 1 == 1:
+            diff += 1
+        xor >>= 1
+    if diff <= k:
+        answer += 1
+print(answer)
