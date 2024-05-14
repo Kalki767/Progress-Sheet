e433f32443b8ecba461901ba0851613602641900
Kalkidan Kidane <137619853+Kalki767@users.noreply.github.com>
2024-05-14 09:31:11 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 14-May-2024/Cirno's Perfect Bitmasks Classroom 148527.py
@@ -0,0 +1,20 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+def solve():
+    n = int(input())
+    if n & n-1 == 0:
+        if n != 1:
+            print(n+1)
+            return
+        print(3)
+    else:
+        length = n.bit_length()
+        for i in range(length):
+            if n & 1 << i and n ^ 1 << i:
+                print(1<<i)
+                return
+t = int(input())
+for _ in range(t):
+    solve()
+        
+            
