437a85d44ffa12131965cf5ec814e81e09a9ff7d
Kalki767 <137619853+Kalki767@users.noreply.github.com>
2024-05-17 14:09:30 +0300

    Modified

--- Dynamic_Programming/A_Frog_1.py
+++ Dynamic_Programming/A_Frog_1.py
@@ -1,15 +1,13 @@
 n = int(input())
 nums = list(map(int,input().split()))
-memory = {}
 
-def dp(index):
-    if index == len(nums)-1:
-        return 0
-    if index not in memory:
-        one_step = abs(nums[index+1]-nums[index]) + dp(index+1)
-        two_step = float('inf')
-        if index + 2 < len(nums):
-            two_step = abs(nums[index+2]-nums[index]) + dp(index+2)
-        memory[index] = min(one_step,two_step)
-    return memory[index]
-print(dp(0))
+dp = [float('inf') for _ in range(n)]
+dp[-1] = 0
+for index in range(n-2,-1,-1):
+    one_step = abs(nums[index+1]-nums[index]) + dp[index+1]
+    two_step = float('inf')
+    if index + 2 < len(nums):
+        two_step = abs(nums[index+2]-nums[index]) + dp[index+2]
+    dp[index] = min(one_step,two_step)
+print(dp[0])
+
