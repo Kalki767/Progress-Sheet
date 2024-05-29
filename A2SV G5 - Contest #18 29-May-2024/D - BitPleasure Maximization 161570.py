e529d184782cafaeb1cc066c853481f1673fe41c
Kalkidan Kidane <137619853+Kalki767@users.noreply.github.com>
2024-05-29 17:07:55 +0300

    Created 'D - BitPleasure Maximization' by A2SV Hub

---
+++ A2SV G5 - Contest #18 29-May-2024/D - BitPleasure Maximization 161570.py
@@ -0,0 +1,52 @@
+# Problem: D - BitPleasure Maximization - https://codeforces.com/gym/526229/problem/D
+
+class TrieNode:
+    def __init__(self):
+        self.children = [None] * 2
+        self.is_end_of_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for ch in word:
+            index = int(ch)
+            if not node.children[index]:
+                node.children[index] = TrieNode()
+            node = node.children[index]
+        node.is_end_of_word = True
+
+    def maximum_xor(self,word):
+        current = self.root
+        answer = 0
+        for index,bit in enumerate(word):
+            i = int(bit)
+            opposite = 1 - i
+            if current.children[opposite] is not None:
+                answer = answer | (1<<63-index)
+                current = current.children[opposite]
+            else:
+                current = current.children[i]
+        return answer
+
+
+trie = Trie()
+n = int(input())
+nums = list(map(int,input().split()))
+nums.append(0)
+xor = [0]
+for num in nums:
+    xor.append(xor[-1]^num)
+for num in xor: 
+    binary_str = format(num,'064b')
+    trie.insert(binary_str)
+answer = 0
+current = 0
+for index in range(n,-1,-1):
+    current = current ^ nums[index]
+    binary_str = format(current,'064b')
+    answer = max(trie.maximum_xor(binary_str),answer)
+ 
+print(answer)
