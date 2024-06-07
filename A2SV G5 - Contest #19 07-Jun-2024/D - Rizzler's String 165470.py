c49899956694dcdba961781e606316863a23d4ce
Kalkidan Kidane <137619853+Kalki767@users.noreply.github.com>
2024-06-07 20:11:27 +0300

    Created 'D - Rizzler's String' by A2SV Hub

---
+++ A2SV G5 - Contest #19 07-Jun-2024/D - Rizzler's String 165470.py
@@ -0,0 +1,13 @@
+# Problem: D - Rizzler's String - https://codeforces.com/gym/527294/problem/D
+
+s = input()
+answer = 0
+last_b = 0
+mod = 10**9 + 7
+for i in range(len(s)):
+    if s[i] == 'b':
+        last_b = answer
+    elif s[i] == 'a':
+        answer += last_b + 1
+        answer %= mod
+print(answer)
