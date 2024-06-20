a16953e31acd5a4df63049a874ddb155bc7f1c6a
Kalkidan Kidane <137619853+Kalki767@users.noreply.github.com>
2024-06-20 23:01:07 +0300

    Created 'A - abbccc' by A2SV Hub

---
+++ A2SV G5 - Contest #21 20-Jun-2024/A - abbccc 170879.py
@@ -0,0 +1,12 @@
+# Problem: A - abbccc - https://codeforces.com/gym/530187/problem/A
+
+n = int(input())
+t = input()
+s = []
+index = 0
+current = 0
+while index < len(t):
+    current += 1
+    s.append(t[index])
+    index += current
+print(''.join(s))
