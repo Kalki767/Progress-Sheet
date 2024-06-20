92934a017870331fa6d43a4aa90c395182ba48ed
Kalkidan Kidane <137619853+Kalki767@users.noreply.github.com>
2024-06-20 23:01:16 +0300

    Created 'B - Distinct Digits' by A2SV Hub

---
+++ A2SV G5 - Contest #21 20-Jun-2024/B - Distinct Digits 170880.py
@@ -0,0 +1,22 @@
+# Problem: B - Distinct Digits - https://codeforces.com/gym/530187/problem/B
+
+import math
+
+
+def solve():
+    s = int(input())
+    answer = []
+    current = 9
+    while s > current:
+        answer.append(current)
+        s -= current
+        current -= 1
+    answer.append(s)
+    answer.reverse()
+    res = 0
+    for i in answer:
+        res = res * 10 + i
+    print(res)
+t = int(input())
+for _ in range(t):
+    solve()
