16efaa6d0f72567abae420b054e4da2fbd5a45a3
Kalkidan Kidane <137619853+Kalki767@users.noreply.github.com>
2024-07-05 15:43:04 -0400

    Created 'Fibonacci Number' by A2SV Hub

---
+++ 05-Jul-2024/Fibonacci Number 176748.py
@@ -0,0 +1,13 @@
+# Problem: Fibonacci Number - https://leetcode.com/problems/fibonacci-number/
+
+class Solution:
+    def fib(self, n: int) -> int:
+        '''Approach: Recursion'''
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        return self.fib(n-1) + self.fib(n-2)
+        #Time Complexity: O(2^n)
+        #Space Complexity: O(n)
+
