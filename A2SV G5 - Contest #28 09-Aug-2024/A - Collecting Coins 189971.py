154de04e03e8a25300d5e99eca0e6d7f577ef661
Kalkidan Kidane <137619853+Kalki767@users.noreply.github.com>
2024-08-09 16:44:27 +0300

    Created 'A - Collecting Coins' by A2SV Hub

---
+++ A2SV G5 - Contest #28 09-Aug-2024/A - Collecting Coins 189971.py
@@ -0,0 +1,10 @@
+# Problem: A - Collecting Coins - https://codeforces.com/gym/540354/problem/A
+
+t = int(input())
+for _ in range(t):
+    a,b,c,n = map(int,input().split())
+    val = (a+b+c+n)
+    if val % 3 == 0 and (val//3 >= max(a,b,c)):
+        print('YES')
+    else:
+        print('NO')
