cbc1c20ab479f9da739ab70c258c04e1bc7d79e1
Kalkidan Kidane <137619853+Kalki767@users.noreply.github.com>
2024-08-09 16:48:06 +0300

    Created 'B - Fasika and Children' by A2SV Hub

---
+++ A2SV G5 - Contest #27 09-Aug-2024/B - Fasika and Children 189974.py
@@ -0,0 +1,13 @@
+# Problem: B - Fasika and Children - https://codeforces.com/gym/538762/problem/B
+
+import math
+n,m = map(int,input().split())
+candies = list(map(int,input().split()))
+for i in range(n):
+    curr = math.ceil(candies[i]/m)
+    candies[i] = curr
+answer = 0
+for i in range(n):
+    if candies[i] >= candies[answer]:
+        answer = i
+print(answer+1)
