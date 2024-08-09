e1c672f09a0334b5fd70b9f174492bb432f54100
Kalkidan Kidane <137619853+Kalki767@users.noreply.github.com>
2024-08-09 16:44:18 +0300

    Created 'B - K-divisible Sum' by A2SV Hub

---
+++ A2SV G5 - Contest #28 09-Aug-2024/B - K-divisible Sum 189970.py
@@ -0,0 +1,14 @@
+# Problem: B - K-divisible Sum - https://codeforces.com/gym/540354/problem/B
+
+import math
+
+
+def solve():
+    n, k = map(int,input().split())
+    diff = math.ceil(n/k)
+    k *= diff
+    answer = math.ceil(k/n)
+    print(answer)
+t = int(input())
+for _ in range(t):
+    solve()
