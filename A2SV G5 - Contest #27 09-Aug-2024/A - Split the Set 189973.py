82126360b7404864de9ceb5b9b5698fd62acd24b
Kalkidan Kidane <137619853+Kalki767@users.noreply.github.com>
2024-08-09 16:48:00 +0300

    Created 'A - Split the Set' by A2SV Hub

---
+++ A2SV G5 - Contest #27 09-Aug-2024/A - Split the Set 189973.py
@@ -0,0 +1,16 @@
+# Problem: A - Split the Set - https://codeforces.com/gym/538762/problem/A
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    nums = list(map(int,input().split()))
+    counter = {0:0,1:0}
+    for num in nums:
+        if num % 2 == 0:
+            counter[0] += 1
+        else:
+            counter[1] += 1
+    if counter[0] == counter[1]:
+        print('Yes')
+    else:
+        print('No')
