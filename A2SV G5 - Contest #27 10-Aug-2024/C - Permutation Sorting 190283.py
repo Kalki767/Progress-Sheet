2465055df43ee35f7068c19c4b1fd06a6205bbb0
Kalkidan Kidane <137619853+Kalki767@users.noreply.github.com>
2024-08-10 11:22:51 +0300

    Created 'C - Permutation Sorting' by A2SV Hub

---
+++ A2SV G5 - Contest #27 10-Aug-2024/C - Permutation Sorting 190283.py
@@ -0,0 +1,16 @@
+# Problem: C - Permutation Sorting - https://codeforces.com/gym/538762/problem/C
+
+def solve():
+    n = int(input())
+    permutation = list(map(int,input().split()))
+    previous = set()
+    answer = 0
+    for num in permutation:
+        if num+1 in previous:
+            answer += 1
+        previous.add(num)
+    print(answer)
+
+t = int(input())
+for _ in range(t):
+    solve()
