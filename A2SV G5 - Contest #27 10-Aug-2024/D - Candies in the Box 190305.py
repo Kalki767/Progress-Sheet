e382fe0284908c333844f0ddf25ce8e96afa6d62
Kalkidan Kidane <137619853+Kalki767@users.noreply.github.com>
2024-08-10 15:00:37 +0300

    Created 'D - Candies in the Box' by A2SV Hub

---
+++ A2SV G5 - Contest #27 10-Aug-2024/D - Candies in the Box 190305.py
@@ -0,0 +1,25 @@
+# Problem: D - Candies in the Box - https://codeforces.com/gym/538762/problem/D
+
+def check(n,k):
+    remaining = n
+    vasya = 0
+    while remaining > 0:
+        k = min(k,remaining)
+        vasya += k
+        remaining -= k
+        remaining -= (remaining//10)
+
+    return vasya * 2 >= n
+
+n = int(input())
+left, right = 1, n
+answer = n
+while left <= right:
+    mid = left + (right - left)//2
+    if check(n,mid):
+        answer = min(answer,mid)
+        right = mid - 1
+    else:
+        left = mid + 1
+        
+print(answer)
