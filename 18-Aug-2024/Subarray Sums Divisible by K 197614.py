14c812886d8152fd8b7f4150a87179d0fcf4c981
Kalkidan Kidane <137619853+Kalki767@users.noreply.github.com>
2024-08-18 18:39:55 +0300

    Created 'Subarray Sums Divisible by K' by A2SV Hub

---
+++ 18-Aug-2024/Subarray Sums Divisible by K 197614.py
@@ -0,0 +1,28 @@
+# Problem: Subarray Sums Divisible by K - https://leetcode.com/problems/subarray-sums-divisible-by-k/
+
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        '''Approach: prefix sum here we are trying to find a subarray whose
+        sum is divisible by k to achieve that we used a prefix sum to store
+        the current sum and to find a particular subarray sum we need to
+        subtract our starting point from our end point in our prefix sum
+        so that our subarray to be divisible by k the two end points need
+        to have the same modulo so while going once through our list we
+        check if we have gotten the same modulo before if so add it to our
+        result and add the key to our dictionary'''
+        #Step1: create our modulo dictionary to store the modulos we have encountered so far
+        modulo = defaultdict(int)
+        modulo[0] = 1
+        current_sum = 0
+        result = 0
+
+        '''Step2: iterate through the array while checking if the current sum
+        modulo k exists in our dictionary if it does update our result'''
+        for num in nums:
+            current_sum += num
+            remainder = current_sum%k
+            result += modulo[remainder]
+            modulo[remainder] += 1
+        return result
+        #Time Complexity:O(n)
+        #Space Complexity:O(n)
