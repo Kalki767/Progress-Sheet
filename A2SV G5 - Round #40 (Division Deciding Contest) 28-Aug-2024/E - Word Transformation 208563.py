bc963561ae6b1d0c7ab146782500f3a3d3381bc1
Kalkidan Kidane <137619853+Kalki767@users.noreply.github.com>
2024-08-28 10:50:09 +0300

    Created 'E - Word Transformation' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 28-Aug-2024/E - Word Transformation 208563.py
@@ -0,0 +1,22 @@
+# Problem: E - Word Transformation - https://codeforces.com/gym/543431/problem/E
+
+def solve():
+    initial, final = input().split()
+    left, right = len(initial) - 1, len(final) - 1
+    deleted = set()
+    while left >= 0 and right >= 0:
+        if final[right] in deleted:
+            return "NO"
+        elif initial[left] != final[right]:
+            deleted.add(initial[left])
+        else:
+            right -= 1
+        left -= 1
+    if right < 0:
+        return "YES"
+    return "NO"
+
+t = int(input())
+for _ in range(t):
+    result = solve()
+    print(result)
