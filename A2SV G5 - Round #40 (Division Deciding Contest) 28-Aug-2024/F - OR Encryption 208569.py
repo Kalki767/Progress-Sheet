4a9ab04d76224dbd9e40f390b5e3ce13b25b876f
Kalkidan Kidane <137619853+Kalki767@users.noreply.github.com>
2024-08-28 11:28:49 +0300

    Created 'F - OR Encryption' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 28-Aug-2024/F - OR Encryption 208569.py
@@ -0,0 +1,29 @@
+# Problem: F - OR Encryption - https://codeforces.com/gym/543431/problem/F
+
+def solve():
+    n = int(input())
+    matrix = []
+    for _ in range(n):
+        cur = list(map(int,input().split()))
+        matrix.append(cur)
+    setted = (2**30) - 1
+    locks = [setted for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                continue
+            locks[i] &= matrix[i][j]
+            locks[j] &= matrix[i][j]
+    for i in range(n):
+        for j in range(n):
+            if i != j and matrix[i][j] != (locks[i] | locks[j]):
+                return 'NO'
+    return locks
+t = int(input())
+for _ in range(t):
+    result = solve()
+    if result == 'NO':
+        print(result)
+    else:
+        print('YES')
+        print(*result)
