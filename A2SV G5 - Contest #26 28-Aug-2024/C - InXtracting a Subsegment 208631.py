454059e8cf8a2d11fb36ea37ae6e6debd7665665
Kalkidan Kidane <137619853+Kalki767@users.noreply.github.com>
2024-08-28 16:50:47 +0300

    Created 'C - InXtracting a Subsegment' by A2SV Hub

---
+++ A2SV G5 - Contest #26 28-Aug-2024/C - InXtracting a Subsegment 208631.py
@@ -0,0 +1,10 @@
+# Problem: C - InXtracting a Subsegment - https://codeforces.com/gym/537362/problem/C
+
+n, k = map(int,input().split())
+nums = list(map(int,input().split()))
+if k == 1:
+    print(min(nums))
+elif k == 2:
+    print(max(nums[0],nums[-1]))
+else:
+    print(max(nums))
