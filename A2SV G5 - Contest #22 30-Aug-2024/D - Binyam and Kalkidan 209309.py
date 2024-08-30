69cbda65a17248e6baa537d2f142b17088d2cda7
Kalkidan Kidane <137619853+Kalki767@users.noreply.github.com>
2024-08-30 10:16:42 +0300

    Created 'D - Binyam and Kalkidan' by A2SV Hub

---
+++ A2SV G5 - Contest #22 30-Aug-2024/D - Binyam and Kalkidan 209309.py
@@ -0,0 +1,18 @@
+# Problem: D - Binyam and Kalkidan - https://codeforces.com/gym/531455/problem/D
+
+n = int(input())
+number = input()
+digits = []
+for digit in number:
+    if digit == '4':
+        digits.extend(['3','2','2'])
+    elif digit == '6':
+        digits.extend(['5','3'])
+    elif digit == '8':
+        digits.extend(['2','2','2','7'])
+    elif digit == '9':
+        digits.extend(['3','3','2','7'])
+    elif digit in '2357':
+        digits.extend(digit)
+digits.sort(reverse=True)
+print(int(''.join(digits)))
